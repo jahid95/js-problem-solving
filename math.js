16664e530d76b92cd241b536683f4c1c475693a1
Anwar <anwarjahid449@gmail.com>
2023-02-02 17:13:02 +0600

    swap with temp and destructuring

---
+++ math.js
@@ -0,0 +1,12 @@
+const number = 88.88;
+const absNumber = Math.abs(number);
+const roundNumber = Math.round(number);
+const floorNumber = Math.floor(number);
+const ceilNumber = Math.ceil(number);
+const randomNumber = Math.ceil( Math.random()*6);
+// console.log(randomNumber);
+
+for(let i=0; i<20; i++){
+    const randomNum = Math.ceil(Math.random()*6);
+    console.log(randomNum);
+}
--- reverse.js
+++ reverse.js
@@ -2,9 +2,10 @@ function getReverse(text){
 
     for(var i =text.length-1; i>=0; i-- ){
         const element = text[i];
-        console.log(element);
+        const sentense = element.join(' ');
     }
+    return sentense;
 }
 const myText ='My name is anwar Jahid';
 const output = getReverse(myText);
-// console.log(output);
+console.log(output);
